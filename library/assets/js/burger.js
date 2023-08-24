1a97f104c41aa8b202bca67a20a41b096d16b54f
Alexey888888 <brdkmail@gmail.com>
2023-08-24 11:13:24 +0300

    feat: add dropMenu

--- library/assets/js/burger.js
+++ library/assets/js/burger.js
@@ -3,27 +3,71 @@ const menu = document.querySelector('.menu');
 const menuLink = document.querySelectorAll('.menu__link');
 const burgerOverflow = document.querySelector('.burger-overflow');
 
-
-
-
 const clickHandler = () => {
-    menu.classList.toggle('menu-active');
-    document.querySelector('.burger').classList.toggle('burger-active');
-    document.querySelector('body').classList.toggle('scroll-lock');
-} 
+  menu.classList.toggle('menu-active');
+  document.querySelector('.burger').classList.toggle('burger-active');
+  document.querySelector('html').classList.toggle('scroll-lock');
+  if (dropMenu.classList.contains('dropVisible')) closeDropMenu();
+};
+
+function closeBurger() {
+  menu.classList.remove('menu-active');
+  document.querySelector('.burger').classList.remove('burger-active');
+  document.querySelector('html').classList.remove('scroll-lock');
+}
 
 burger.addEventListener('click', clickHandler);
 
-menuLink.forEach((item) => item.addEventListener('click', () => {
+menuLink.forEach((item) =>
+  item.addEventListener('click', () => {
     if (burger.classList.contains('burger-active')) {
-        clickHandler();
+      clickHandler();
     }
-}));
-
-
+  })
+);
 
 burgerOverflow.addEventListener('click', () => {
-    menu.classList.remove('menu-active');
-    document.querySelector('.burger').classList.remove('burger-active');
-    document.querySelector('body').classList.remove('scroll-lock');    
+  menu.classList.remove('menu-active');
+  document.querySelector('.burger').classList.remove('burger-active');
+  document.querySelector('html').classList.remove('scroll-lock');
+  if (dropMenu.classList.contains('dropVisible')) closeDropMenu();
+});
+
+//-----USER-ICON-----//
+
+const userIcon = document.querySelector('.header__icon');
+const dropMenu = document.querySelector('.header__dropMenu');
+
+userIcon.addEventListener('click', handlerDropMenu);
+
+function handlerDropMenu() {
+  if (!dropMenu.classList.contains('dropVisible')) {
+    openDropMenu();
+  } else if (dropMenu.classList.contains('dropVisible')) {
+    closeDropMenu();
+  }
+}
+
+function openDropMenu() {
+  dropMenu.classList.add('dropVisible');
+  closeBurger();
+  event.stopPropagation();
+}
+
+function closeDropMenu() {
+  dropMenu.classList.add('dropHidden');
+  dropMenu.addEventListener('animationend', cleanClassList);
+  function cleanClassList() {
+    dropMenu.classList.remove('dropVisible');
+    dropMenu.classList.remove('dropHidden');
+    dropMenu.removeEventListener('animationend', cleanClassList);
+  }
+}
+
+document.querySelector('body').addEventListener('click', () => {
+  if (dropMenu.classList.contains('dropVisible')) closeDropMenu();
+});
+
+dropMenu.addEventListener('click', () => {
+  event.stopPropagation();
 });
