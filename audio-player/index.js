8dfff0540a3c825a0b549b1201c0d94b7c556dcd
Alexey888888 <brdkmail@gmail.com>
2023-09-20 13:15:31 +0300

    feat: add function for current and duration time

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,11 +1,15 @@
 const btnPlay = document.querySelector('.play');
 const btnPause = document.querySelector('.pause');
+const currentTime = document.querySelector('.current-time');
+const durationTime = document.querySelector('.duration-time');
+
 const audio = new Audio();
 
 function playAudio() {
   audio.src = './assets/audio/Kavinsky_Nightcall.mp3';
   audio.currentTime = 0;
   audio.play();
+  setCurrentTime();
 }
 
 function pauseAudio() {
@@ -26,3 +30,23 @@ btnPause.addEventListener('click', () => {
   pauseAudio();
   togglePlayBtn();
 });
+
+// PROGRESS-BAR START
+
+function setCurrentTime() {
+  setInterval(() => {
+    currentTime.innerHTML = getTime(audio.currentTime);
+  }, 1000);
+}
+
+audio.addEventListener('loadeddata', () => {
+  durationTime.textContent = getTime(audio.duration);
+});
+
+function getTime(time) {
+  const minutes = parseInt(parseInt(time) / 60);
+  const seconds = (parseInt(time) - minutes * 60) % 60;
+  return `${minutes}:${String(seconds).padStart(2, 0)}`;
+}
+
+// PROGRESS-BAR END
