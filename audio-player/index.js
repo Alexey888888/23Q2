179eb86c245ce14a45b8480eb904e8da00895ad0
Alexey888888 <brdkmail@gmail.com>
2023-09-21 11:06:01 +0300

    feat: change div to input, add rewind function and handler for click on time-line

--- audio-player/index.js
+++ audio-player/index.js
@@ -4,26 +4,47 @@ const currentTime = document.querySelector('.current-time');
 const durationTime = document.querySelector('.duration-time');
 
 let currentTimeTemp = null;
+let pos = null;
+let intervalId = null;
+let isPlay = false;
+let afterRewind = false;
 
 const audio = new Audio();
 
-function playAudio() {
+function preloadAudio() {
   audio.src = './assets/audio/Kavinsky_Nightcall.mp3';
+}
+preloadAudio();
+
+function playAudio() {
   if (currentTimeTemp) {
     audio.currentTime = currentTimeTemp;
   } else audio.currentTime = 0;
   audio.play();
   setCurrentTime();
+  moveProgressLine();
+  isPlay = true;
 }
 
 function pauseAudio() {
   audio.pause();
+  if (currentTimeTemp && afterRewind === true)
+    audio.currentTime = currentTimeTemp;
+  afterRewind = false;
   currentTimeTemp = audio.currentTime;
+  isPlay = false;
 }
 
 function togglePlayBtn() {
   btnPlay.classList.toggle('none');
   btnPause.classList.toggle('none');
+  if (isPlay) {
+    btnPlay.classList.add('none');
+    btnPause.classList.remove('none');
+  } else {
+    btnPlay.classList.remove('none');
+    btnPause.classList.add('none');
+  }
 }
 
 btnPlay.addEventListener('click', () => {
@@ -44,16 +65,15 @@ function setCurrentTime() {
   }, 100);
 }
 
-function moveProgressLine() {
-  setInterval(() => {
-    document.querySelector('.progress-line').style.width =
-      (audio.currentTime / audio.duration) * 100 + '%';
-    document.querySelector('.progress-point').style.marginLeft =
-      (audio.currentTime / audio.duration) * 100 + '%';
+function moveProgressLine(pos) {
+  intervalId = setInterval(() => {
+    // document.querySelector('.progress-line').style.width =
+    //   (audio.currentTime / audio.duration) * 100 + '%';
+
+    document.querySelector('.time-line').value =
+      (audio.currentTime / audio.duration) * 1000;
   }, 100);
 }
-
-moveProgressLine();
 
 audio.addEventListener('loadeddata', () => {
   durationTime.textContent = getTime(audio.duration);
@@ -65,4 +85,28 @@ function getTime(time) {
   return `${minutes}:${String(seconds).padStart(2, 0)}`;
 }
 
+// function timeLineClickHandler() {
+//   document.querySelector('.time-line').addEventListener('click', () => {
+//     pauseAudio();
+//     clearInterval(intervalId);
+//     document.querySelector('.time-line').oninput = function () {
+//       pos = this.value;
+//       currentTimeTemp = (pos * audio.duration) / 1000;
+//       console.log(currentTimeTemp);
+//       playAudio();
+//     };
+//     playAudio();
+//   });
+// }
+
+document.querySelector('.time-line').oninput = function () {
+  pos = this.value;
+  currentTimeTemp = (pos * audio.duration) / 1000;
+  console.log(currentTimeTemp);
+  if (isPlay) playAudio();
+  afterRewind = true;
+  if (!isPlay) pauseAudio();
+};
+
+// timeLineClickHandler();
 // PROGRESS-BAR END
