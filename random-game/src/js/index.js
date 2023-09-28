7bb569408980706b2cc3673b3137820bbf1b86e7
Alexey888888 <brdkmail@gmail.com>
2023-09-28 17:18:58 +0300

    feat: add bombs to board

--- random-game/src/js/index.js
+++ random-game/src/js/index.js
@@ -15,6 +15,24 @@ function createMatrix(width = 10, height = 10, bombs = 10) {
   return matrix;
 }
 
+const getRandom = (min = 0, max = 9) => {
+  let num = Math.floor(min + Math.random() * (max - min + 1));
+  return num;
+};
+
+function createMatrixBombs() {
+  const matrixBombs = [];
+  const bombArrTemp = [];
+  const create2 = () => {
+    const bomb = [getRandom(), getRandom()];
+    if (!bombArrTemp.includes(bomb.join(''))) matrixBombs.push(bomb);
+    bombArrTemp.push(bomb.join(''));
+    if (matrixBombs.length < 10) create2();
+  };
+  create2();
+  return matrixBombs;
+}
+
 function fillBoard() {
   const matrix = createMatrix();
   for (let i = 0; i < matrix.length; i++) {
@@ -25,10 +43,20 @@ function fillBoard() {
       cell.className = 'cell';
       border.append(cell);
       board.append(border);
-      //cell.innerHTML = `[${i}, ${j}]`;
+      cell.innerHTML = `[${i}, ${j}]`; //
       cell.dataset.id = `[${i}, ${j}]`;
     }
   }
+  function addBombs() {
+    const matrixBombs = createMatrixBombs();
+    console.log(matrixBombs);
+    for (let i = 0; i < matrixBombs.length; i++) {
+      document.querySelector(
+        `[data-id="[${matrixBombs[i][0]}, ${matrixBombs[i][1]}]"]`
+      ).innerHTML = '<span class="bomb">💩</span>';
+    }
+  }
+  addBombs();
 }
 
 fillBoard();
